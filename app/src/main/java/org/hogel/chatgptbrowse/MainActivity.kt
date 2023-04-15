3bba57d051a49f42b97e7454c385f54aa2863cfa
hogelog <konbu.komuro@gmail.com>
2023-04-15 19:47:58 +0900

    Add CustomTabs support and deep link to ChatGPT

--- app/src/main/java/org/hogel/chatgptbrowse/MainActivity.kt
+++ app/src/main/java/org/hogel/chatgptbrowse/MainActivity.kt
@@ -1,11 +1,16 @@
 package org.hogel.chatgptbrowse
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabsIntent
 
 class MainActivity : AppCompatActivity() {
+    val CHATGPT_URL = Uri.parse("https://chat.openai.com/")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val customTabsIntent = CustomTabsIntent.Builder().build()
+        customTabsIntent.launchUrl(this, CHATGPT_URL)
     }
 }
