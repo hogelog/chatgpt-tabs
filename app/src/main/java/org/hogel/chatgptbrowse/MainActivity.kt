6bc52ce7c62f2b54be513c4ceee639ff4a7f921b
hogelog <konbu.komuro@gmail.com>
2023-04-23 12:06:23 +0900

    Configure toolbar

--- app/src/main/java/org/hogel/chatgptbrowse/MainActivity.kt
+++ app/src/main/java/org/hogel/chatgptbrowse/MainActivity.kt
@@ -2,15 +2,33 @@ package org.hogel.chatgptbrowse
 
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 
 class MainActivity : AppCompatActivity() {
-    val CHATGPT_URL = Uri.parse("https://chat.openai.com/")
+    private val CHATGPT_URL = Uri.parse("https://chat.openai.com/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val customTabsIntent = CustomTabsIntent.Builder().build()
-        customTabsIntent.launchUrl(this, CHATGPT_URL)
+
+        val colorSchemeParams = CustomTabColorSchemeParams.Builder()
+            .setToolbarColor(getColor(R.color.toolbar))
+            .build()
+
+        val customTabsIntent = CustomTabsIntent.Builder()
+            .setUrlBarHidingEnabled(true)
+            .setShareState(CustomTabsIntent.SHARE_STATE_OFF)
+            .setShowTitle(true)
+            .setDefaultColorSchemeParams(colorSchemeParams)
+            .build()
+        customTabsIntent.intent.data = CHATGPT_URL
+
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                finish()
+            }
+        }.launch(customTabsIntent.intent)
     }
 }
